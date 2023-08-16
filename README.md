c4f59b5311270bc284f9384d4916901834019f52
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2023-08-16 01:15:51 -0300

    feat finish config eslint and dotenv

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - yarn add sequelize-cli -D
   * linha de comando que ajuda criar codigos mais rapidos, migrations, models
 - yarn add nodemon -D
+- yarn add eslint -D
 
 <h3>Run project</h3>
 - yarn dev - development environment
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
-const PORT = 3000;
+const { PORT } = process.env;
 
 app.use(express.json());
-
+app.use(routes);
 app.listen(PORT, () => {
-  console.log("server is on in port 3000");
+  console.log(`server is on in port ${PORT}`);
 });
